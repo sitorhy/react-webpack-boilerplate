d5f5e7aa102da50994e36d42e860bb4a43275366
sitorhy <876434932@qq.com>
2018-12-11 01:00:24 +0800

    全局、局部样式示例

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 import {Link} from "react-router";
 
+// global styles
+import "./global.less"
+
+import styles from "./todo.scope.less";
+
 class Home extends React.Component
 {
     render()
     {
         return (
             <div>
-                <div>
+                <div className={"home-page"}>
                     <p>Home Page</p>
                     <div>
                         <Link to={"/home/welcome"}>Welcome</Link>
@@ -16,7 +21,7 @@ class Home extends React.Component
                         <Link to={"/home/about"}>About</Link>
                     </div>
                     <div>
-                        <Link to={"/home/todo"}>ToDo</Link>
+                        <Link className={styles.todo} to={"/home/todo"}>ToDo</Link>
                     </div>
                     <div>
                         {this.props.children}
